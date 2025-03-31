bf741c29e6577deaae79ce30426b2a176a47a215
24LP8280 24LP8280 <24LP8280@G303-CC0161.local>
2025-03-31 12:40:52 +0800

    data up

---
+++ src/app/components/FoodData.ts
@@ -0,0 +1,95 @@
+export  const FoodData = [
+    {
+        id: 1,
+        ItemsName: "Appetizers",
+        foodItems: [
+            {
+                id: 101,
+                name: "Finger food",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.",
+            },
+            {
+                id: 102,
+                name: "Cranberry Brie Bites",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            },
+            {
+                id: 103,
+                name: "Sunshine Stackers ",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            },
+            {
+                id: 104,
+                name: "Brie Crostini Appetizer ",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            },
+       
+        ]
+    },
+    {
+        id:2,
+        ItemsName:"Salads",
+        foodItems: [
+            {
+                id: 201,
+                name: "Grilled Chicken cobb salad",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.",
+            },
+            {
+                id: 202,
+                name: "Burrata Caprese",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            },
+            {
+                id: 203,
+                name: "betroot and orange salad",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            },
+        ]
+    },
+    {
+        id: 3,
+        ItemsName: "Lunch favorites",
+        foodItems: [
+            {
+                id: 301,
+                name: "Sunshine Stackers",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar.",
+            },
+            {
+                id: 302,
+                name: "Cranberry Brie Bites",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            },
+            {
+                id: 303,
+                name: "Sunshine Stackers ",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            },
+            {
+                id: 304,
+                name: "Brie Crostini Appetizer ",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            },
+            {
+                id: 305,
+                name: "Grilled chicken ",
+                price: 12.99,
+                ingredients: "Fluffy pancakes stacked with fruits, cream, syrup, and powdered sugar."
+            }
+        ]
+    },
+]
+
+export default FoodData
--- src/app/components/HeaderComp.tsx
+++ src/app/components/HeaderComp.tsx
@@ -13,7 +13,8 @@ const HeaderComp = () => {
         </div>
         <div className='flex gap-3 mr-[88px]'>
           <button className='text-[#EF4444] w-[200px] mt-4 h-[20px] bg-white rounded-full text-[10px] flex'>
-            <MapPin className='rounded-full w-[29px] h-[29px] p-2' /> Delivery address: Add Location</button>
+            <MapPin className='rounded-full w-[29px] h-[29px] p-2 flex place-self-center' /> <span className='place-self-center'>Delivery address:</span>
+           <span className='text-gray-400 place-self-center'>Add Location <span></span></span> </button>
           <ShoppingCart className='bg-white rounded-full w-[29px] h-[29px] p-2 mt-4' />
           <User className='bg-[#EF4444] text-white rounded-full w-[29px] h-[29px] p-2 mt-4' />
         </div>
